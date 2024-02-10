65040c6fd217624cebb23236acee630f127d472d
Yong Sheng Tan <yongsheng.tan@shareinvestor.com>
2024-02-11 00:53:05 +0800

    update

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/hello/", response_class=HTMLResponse)
+async def hello(request: Request):
+   return templates.TemplateResponse("hello.html", {"request": request})
 
 if __name__=='__main__':
     uvicorn.run('app.main:app', reload=True)
