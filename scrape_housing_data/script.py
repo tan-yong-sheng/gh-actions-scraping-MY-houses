9ea2d69dd999b4a250180a62028506a65d249ecd
Yong Sheng Tan <yongsheng.tan@shareinvestor.com>
2024-02-12 00:09:05 +0800

    update

--- scrape_housing_data/script.py
+++ scrape_housing_data/script.py
@@ -11,58 +11,48 @@ import datetime
 import pytz
 import json
 import requests
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
 import warnings
-import asyncio
 
 warnings.filterwarnings("ignore")
 
-# for you to change easily
 data_folder = 'output/data'
 now = datetime.datetime.now(tz=pytz.timezone("Asia/Kuala_Lumpur"))
 p = pathlib.Path(os.path.dirname(os.path.dirname(__file__)))
 path_to_data = p.joinpath(data_folder, f"{now:%Y-%m-%d}.json")
-print(os.path.dirname(__file__))
-print(path_to_data)
-
-# read data, if needed
 data = []
-if os.path.exists(path_to_data):
-    with open(path_to_data, 'r') as file:
-        data = json.load(file)
 
-# you can also import SoftwareEngine, HardwareType, SoftwareType, Popularity from random_user_agent.params
-# you can also set number of user agents required by providing `limit` as parameter
-
-software_names = [SoftwareName.CHROME.value]
-operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
-
-user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
-
-
-def get_data_from_mudah():
-    """
-    Scrape mudah.my website for property listings, either for sale or rent.
+"""
     
-    :return: Response from the mudah.my API.
-    :rtype: requests.Response
+    The data wil be saved it into a list of JSON objects.
+
+    :param response: 
+    :type response: requests.Response
+    """
+
+
+def scrape_data_from_mudah():
+    """Scrape mudah.my website for property listings, either for sale or rent.
+
+    :return: A JSON response received from web requests 
+    :rtype: dict
     """
     response = requests.get("https://search.mudah.my/v1/search",
                             params={"category": 2000,
                                     "from": 0,
-                                    "limit": 200},
-                            headers={"User-Agent": user_agent_rotator.get_random_user_agent()})
-    return response
-
-
-def scrape_data(response: requests.Response):
-    """Scrape data from the housing website and save it to a list of JSON objects.
-
-    :param response: A JSON response from web requests using the requests module.
-    :type response: requests.Response
-    """
+                                    "limit": 200})
     json_output = response.json()["data"]
+    return json_output
+
+
+def save_data_to_json(json_output):
+    """Save the web requests data in form of 
+
+    :param json_output: _description_
+    :type json_output: _type_
+    """
+    if os.path.exists(path_to_data):
+        with open(path_to_data, 'r') as file:
+            data = json.load(file)   
     data.extend(json_output)
 
 # execute and persist data
@@ -74,14 +64,12 @@ def main():
     2. Save the scraped JSON data into a list.
     3. Persist the data by writing it to a JSON file.
     """
-    mudah_housing_data = get_data_from_mudah()
-    
-    scrape_data(mudah_housing_data) 
+    mudah_housing_data = scrape_data_from_mudah()
+    save_data_to_json(mudah_housing_data)
     
     # persist data
     with open(path_to_data, 'w') as file:
         json.dump(data, file, indent=2)
-
 
 if __name__ == "__main__":
     main()
--- tests/scrape_housing_data/test_script.py
+++ tests/scrape_housing_data/test_script.py
@@ -3,7 +3,16 @@ test_script.py
 ==========================
 To test scrape_housing_data/script.py file
 """
+import requests
+from scrape_housing_data.script import scrape_data_from_mudah
 
-def test_scrape_data():
-  pass
+def test_scrape_data_from_mudah(mocker):
+  requests_mock = mocker.patch("scrape_housing_data.script.requests.get")
+  requests_mock.return_value = requests.get("https://search.mudah.my/v1/search",
+                            params={"category": 2000,
+                                    "from": 0,
+                                    "limit": 200})
+  assert type(scrape_data_from_mudah()) == dict
+
+  
 
