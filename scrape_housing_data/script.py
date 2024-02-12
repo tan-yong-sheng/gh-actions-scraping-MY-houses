42a30eb57c731534d0e5e83371f9ad59263136b2
Yong Sheng Tan <yongsheng.tan@shareinvestor.com>
2024-02-13 01:15:06 +0800

    update

--- scrape_housing_data/script.py
+++ scrape_housing_data/script.py
@@ -19,7 +19,6 @@ data_folder = 'output/data'
 now = datetime.datetime.now(tz=pytz.timezone("Asia/Kuala_Lumpur"))
 p = pathlib.Path(os.path.dirname(os.path.dirname(__file__)))
 path_to_data = p.joinpath(data_folder, f"{now:%Y-%m-%d}.json")
-data = []
 
 def scrape_data_from_mudah() -> None:
     """Scrape mudah.my website for property listings, either for sale or rent.
@@ -32,16 +31,23 @@ def scrape_data_from_mudah() -> None:
                                     "from": 0,
                                     "limit": 200})
     return response
-    
-    
-def save_data_to_json(response, data = []):
+ 
+
+def save_data_to_json(response):
     json_output = response.json()["data"]
     
+    # Initialize an empty list or load existing data from the file
+    data = []
     if os.path.exists(path_to_data):
         with open(path_to_data, 'r') as file:
-            data = json.load(file)   
+            data = json.load(file)
+    
+    # Extend the existing data with the new JSON output
     data.extend(json_output)
-
+    
+    # Save the updated data back to the file
+    with open(path_to_data, 'w') as file:
+        json.dump(data, file, indent=2)
 
 def main():
     """
@@ -49,14 +55,10 @@ def main():
 
     1. Use `scrape_data` function to scrape data for property listings.
     2. Save the scraped JSON data into a list.
-    3. Persist the data by writing it to a JSON file.putty
+    3. Persist the data by writing it to a JSON file.
     """
     mudah_housing_data = scrape_data_from_mudah()
     save_data_to_json(mudah_housing_data)
-    
-    # persist data
-    with open(path_to_data, 'w') as file:
-        json.dump(data, file, indent=2)
 
 if __name__ == "__main__":
     main()
